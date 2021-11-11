1e9c9b976863a6c15e77236ff8318d7b1046e771
MarianBaba <marianbaba16@gmail.com>
2021-11-11 18:40:36 +0100

    ciao

--- src/main/java/com/marian/AddServlet.java
+++ src/main/java/com/marian/AddServlet.java
@@ -3,22 +3,26 @@ package com.marian;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class AddServlet extends HttpServlet {
-	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException { //SE SCRIVIAMO DOPOST IL METODO VERRA' CHIAMATO SOLAMENTE CON IL METODO POST, si può fare la stessa cosa con doGet
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException { //SE SCRIVIAMO DOPOST IL METODO VERRA' CHIAMATO SOLAMENTE CON IL METODO POST, si può fare la stessa cosa con doGet
 		int i = Integer.parseInt(req.getParameter("num1"));
 		int j = Integer.parseInt(req.getParameter("num2"));
 		
 		int k = i + j;
 		
-		PrintWriter out = res.getWriter();
+		//SESSION MANAGEMENT -> PER CONDIVIDERE DATI FRA SERVLET
 		
-		out.println(k);
 		
+		req.setAttribute("k", k); //se vogliamo passare un dato da un servlet a un altro possiamo metterlo all'interno di un ATTRIBUTI DELLA VARIABILE REQUEST (REQ)
+		
+		//request dispatcher per chiamare un altro servlet da qui -> QUINDI QUESTO E' IL MODO PER CHIAMARE UN SERVLET DA UN SERVLET
+		RequestDispatcher rd = req.getRequestDispatcher("sq");
+		rd.forward(req, res);
 	}
-	
-	
 }
---
+++ src/main/java/com/marian/SqServlet.java
@@ -0,0 +1,22 @@
+package com.marian;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class SqServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+		
+		int k = (int) req.getAttribute("k"); //PER FARE IL FETCH DEL DATO PASSATO ALL'INTERNO DELLA VARIABILE REQ ABBIAMO UN METODO: getAttribute
+		
+		k *= k;
+		
+		PrintWriter out = res.getWriter();
+		out.println("Result is " + k);
+		
+		//THIS IS A NEW SERVLET
+	}
+}
